cb52de845d3e121ba01b4068d1ed387dedc6e85f
[neimanis] <[ernestsneimanis]>
2024-04-09 17:58:38 +0200

    bs icons added to nuxt config

--- nuxtbsc/pages/index.vue
+++ nuxtbsc/pages/index.vue
@@ -16,38 +16,38 @@ onMounted(async() => {
     const response = await posts.getPost({ category:"galleries",slug:"my-test-post"});
     const images = html.extractImageUrls(response.content)
     imageList.value = [...imageList.value,...images];
-    
+
     nextTick(()=>{
 
     })
 })
-
-
 </script>
 
 <template>
- <div class="w-full max-w-full px-8">
-    <Slider ref="slider" v-if="imageList.length > 0"  class="h-72 "> 
-
-  <div v-for="image in imageList" :key="image" class="ratio  h-72  bg-blue-500"
-          
-            >
-                <div class="w-full h-2/3 border-4">
-                     <img :src="image" class="object-cover h-full w-full" alt="">
-                </div>
-                <div class="h-1/3 flex justify-center items-center ">
-                    <div class="px-4 overflow-hidden overflow-ellipsis">
-                        <span class="whitespace-nowrap ">
-                            Some textssssss</span> 
-                    </div>
-                </div>
-            </div>
-
+  <div class="w-full max-w-full px-8">
+    <Slider ref="slider" v-if="imageList.length > 0" class="h-72">
+      <div v-for="image in imageList" :key="image" class="ratio h-72 bg-blue-500">
+        <div class="w-full h-2/3 border-4">
+          <img :src="image" class="object-cover h-full w-full" alt="" />
+        </div>
+        <div class="h-1/3 flex justify-center items-center">
+          <div class="px-4 overflow-hidden overflow-ellipsis">
+            <span class="whitespace-nowrap"> Some textssssss</span>
+          </div>
+        </div>
+      </div>
     </Slider>
-    <div>
-      <button @click="slider.moveRight(0.2)" class="border-4 p-4 ">click</button>
+    <div class="flex justify-between w-full">
+      <div>
+        <button @click="slider.moveRight(0.2)" class="border-4 p-4">click</button>
+      </div>
+      <div>
+        <button @click="slider.moveRight(0.2)" class="border-4 p-4">
+            <i class="bi bi-arrow-right text-black"></i>
+          </button>
+      </div>
     </div>
- </div>
+  </div>
 </template>
 <style scoped>
 .ratio {
